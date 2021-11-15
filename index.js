7a1ae1bba59dfecbb18fce2c769c33f96f1ebef9
abbasShDev <abbas20alzaeem@gmail.com>
2021-11-15 17:42:36 +0300

    Adding https server

--- index.js
+++ index.js
@@ -53,8 +53,8 @@ if (process.env.ENV === "production") {
     app
   );
 
-  httpsServer.listen(443, () => {
-    console.log("HTTPS Server running on port 443");
+  httpsServer.listen(PORT, () => {
+    console.log(`HTTPS Server running on port ${PORT}`);
   });
 } else {
   app.listen(PORT, () => console.log(`App is listening on port: ${PORT}`));
